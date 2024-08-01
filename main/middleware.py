8a6f32af5076fd17c0cf124eeb2576aff4280923
Joe Prakash <joep@nyu.edu>
2024-08-01 18:20:36 -0400

    fix auth issue

--- main/middleware.py
+++ main/middleware.py
@@ -7,6 +7,33 @@ from urllib.parse import parse_qs
 from rest_framework.authtoken.models import Token
 
 
+class TokenAuthMiddleware(BaseMiddleware):
+
+    @database_sync_to_async
+    def get_user(self, token_key):
+        try:
+            token = Token.objects.get(key=token_key)
+            return token.user
+        except Token.DoesNotExist:
+            return AnonymousUser()
+
+    @database_sync_to_async
+    def authenticate(self, token):
+        return self.get_user(token)
+
+    async def __call__(self, scope, receive, send):
+        headers = dict(scope['headers'])
+        token_header = headers.get(b'authorization', b'').decode().split(' ')
+
+        if len(token_header) > 1:
+            token = token_header[1]
+            scope["user"] = await self.get_user(token)
+        else:
+            pass  # Auth Middleware will take care of this
+
+        return await super().__call__(scope, receive, send)
+
+
 class RemoveSlashMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
@@ -24,30 +51,3 @@ class RemoveSlashMiddleware:
         except Exception as e:
             logging.exception(e)
             return HttpResponsePermanentRedirect('/')
-
-
-class TokenAuthMiddleware(BaseMiddleware):
-
-    @database_sync_to_async
-    def get_user(self, token_key):
-        try:
-            token = Token.objects.get(key=token_key)
-            return token.user
-        except Token.DoesNotExist:
-            return AnonymousUser()
-
-    @database_sync_to_async
-    def authenticate(self, token):
-        return self.get_user(token)
-
-    async def __call__(self, scope, receive, send):
-        headers = dict(scope['headers'])
-        token_key = headers.get(b'authorization', b'').decode().split(' ')
-
-        if len(token_key) > 1:
-            token_key = token_key[1]
-            scope["user"] = await self.get_user(token_key)
-        else:
-            scope["user"] = AnonymousUser()
-
-        return await super().__call__(scope, receive, send)
