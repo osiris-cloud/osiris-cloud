ccdf1a32e79bd35a17d08670e22db891ef4011a9
Joe Prakash <joep@nyu.edu>
2024-08-04 00:57:16 -0400

    increase avatar url length

--- apps/oauth/tasks.py
+++ apps/oauth/tasks.py
@@ -9,7 +9,7 @@ import logging
 @shared_task(name='set_profile_avatar')
 def set_profile_avatar(user):
     user = deserialize_obj(user)
-    filename = f'{user.username}-{random_str()}.png'
+    filename = f'{user.username}-{random_str(10)}.png'
     avatar_data = get_avatar(user.first_name, user.last_name)
     upload_to_r2(avatar_data, filename)
     user.avatar = 'https://blob.osiriscloud.io/avatar/' + filename
