ce003c8b2c3935880d2237c13e23872a6cc7fe8c
Joe Prakash <joep@nyu.edu>
2024-08-04 00:58:42 -0400

    add max length to field

--- apps/dns_manager/models.py
+++ apps/dns_manager/models.py
@@ -5,7 +5,7 @@ from ..users.models import User
 
 class Record(models.Model):
     key = models.CharField(max_length=256)
-    value = models.CharField()
+    value = models.CharField(max_length=256)
     ttl = models.PositiveIntegerField()
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='dns_records')
     created_at = models.DateTimeField(auto_now_add=True)
