90e483f503c0b5a5c5b994d8bdd5b904e1b54fbf
Jia Yu Chan <jc11622@nyu.edu>
2024-08-31 13:38:48 -0400

    add secret API url pattern

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -8,6 +8,8 @@ from ..vm import consumers as vm_consumers
 from ..users import api as user_api
 from ..users import consumers as user_consumers
 
+from ..k8s import api as k8s_api
+
 vm_urlpatterns = [
     path('', routes.root),
     path('/token', routes.get_token),
@@ -26,7 +28,13 @@ user_urlpatterns = [
     re_path(r'^/user/(?P<username>[^/]+)$', user_api.user)  # Matches /user/<username>
 ]
 
-urlpatterns = vm_urlpatterns + user_urlpatterns
+secret_urlpatterns = [
+    re_path(r'^/secret$', k8s_api.secret),  # Matches /secret
+    re_path(r'^/secret/(?P<nsid>[^/]+)$', k8s_api.secret),  # Matches /secret/<secretid>
+    re_path(r'^/secret/(?P<nsid>[^/]+)/(?P<secret_name>[^/]+)$', k8s_api.secret)  # Matches /secret/<nsid>/<secret_name>
+]
+
+urlpatterns = vm_urlpatterns + user_urlpatterns + secret_urlpatterns
 
 websocket_urlpatterns = [
     re_path(r'^api/vnc/(?P<vmid>[^/]+)$', vm_consumers.VNCProxyConsumer.as_asgi()),
