bdf82a6450a883d60bf02b165a8dce2298be36ab
Jia Yu Chan <jc11622@nyu.edu>
2024-09-17 17:26:42 -0400

    events api

--- apps/k8s/api.py
+++ apps/k8s/api.py
@@ -8,7 +8,7 @@ from json import dumps as json_dumps
 
 from core.utils import success_message, error_message, random_str
 from .utils import validate_secret_creation, validate_secret_update
-from ..k8s.models import Namespace, NamespaceRoles, Secret
+from ..k8s.models import Namespace, NamespaceRoles, Secret, Event
 from ..users.models import User
 
 def get_user_default_ns(user: User) -> Namespace:
@@ -134,6 +134,46 @@ def secret(request, nsid=None, secret_name=None):
 
                 return JsonResponse(success_message('Delete Secret', {'name': secret_name}))
             
+    except Exception as e:
+        logging.error(str(e))
+        return JsonResponse(error_message(str(e)))
+    
+@api_view(['GET', 'POST'])
+def event(request, event_id=None):
+    try:
+        match request.method:
+            case 'GET':
+                # Get all events for all namespaces user is part of 
+                namespaces = Namespace.objects.filter(namespaceroles__user=request.user)
+                # Get all events for those namespaces
+                events = Event.objects.filter(namespace__in=namespaces)
+                result = []
+                for event in events:
+                    event_info = event.info()
+                    result.append(event_info)
+                
+                return JsonResponse(success_message('Get events', {'events': result}))
+            
+            case 'POST':
+                if not event_id:
+                    return JsonResponse(error_message('No event id provided'))
+                
+                event = Event.objects.filter(event_id=event_id).first()
+
+                if not event:
+                    return JsonResponse(error_message('Event not found'))
+                
+                # Update event status to read
+                try:
+                    event.read = True
+                    event.save()
+
+                except Exception as e:
+                    logging.error(str(e))
+                    return JsonResponse(error_message('Failed to update event status'))          
+
+                return JsonResponse(success_message('Update event status', {'event': event.info()}))
+
     except Exception as e:
         logging.error(str(e))
         return JsonResponse(error_message(str(e)))
