570542a4eb6e8e689e86ab8faf2f61c83aa2c130
Joe Prakash <joep@nyu.edu>
2024-11-06 16:46:13 -0500

    update task

--- apps/k8s/tasks.py
+++ apps/k8s/tasks.py
@@ -1,30 +1,20 @@
-import string
-import random
-import logging
-
 from celery import shared_task
 from celery.contrib.abortable import AbortableAsyncResult
 
-from apps.k8s.utils import make_k8s_client, create_namespace
+from apps.k8s.utils import create_namespace
 from .models import Namespace
-from apps.users.models import Limit
-
-from core.utils import deserialize_obj
 
 
-@shared_task(bind=True, name='init_k8s_for_user', max_retries=3)
-def init_k8s_for_user(self, user):
-    user = deserialize_obj(user)
-    try:
-        v1 = make_k8s_client()
-        ns_name = user.username + '-' + ''.join(
-            [random.choice(string.ascii_lowercase + string.digits) for _ in range(5)])
-        if create_namespace(v1, ns=ns_name):  # Create a namespace for the user
-            ns = Namespace.objects.create(name=ns_name, owner=user, default=True)
-            Limit.objects.create(user=user, cpu=4, memory=2, disk=30, public_ip=0)
-            logging.info(f"Namespace {ns_name} created for user {user.username} with task id {self.request.id}")
-            return True
-        raise Exception
-    except Exception as e:
-        logging.exception(f"Failed to create namespace for user {user.username} with task id {self.request.id}: {e}")
-        self.retry(exc=e, countdown=60)
+@shared_task(bind=True, name='init_k8s_for_user', max_retries=1)
+def init_namespace(self, nsid: str) -> bool:
+    ns = Namespace.objects.get(nsid=nsid)
+    if ns.state == 'active':
+        return True
+
+    if create_namespace(nsid):
+        ns.state = 'active'
+        ns.save()
+        return True
+    else:
+        ns.state = 'error'
+        ns.save()
