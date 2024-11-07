c3891f28139a56be15eb4770049c85282c966056
Joe Prakash <joep@nyu.edu>
2024-11-07 07:56:46 -0500

    add celery

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,9 @@ doppler run -- python manage.py migrate --noinput
 echo "Collect static files"
 doppler run -- python manage.py collectstatic --no-input
 
+echo "Starting Celery worker in background"
+doppler run -- python3 -m celery -A core worker --loglevel INFO &
+
+sleep 2
+
 exec doppler run -- "$@"
