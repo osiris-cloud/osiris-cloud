6a5323bf78a885174e1561c80209fe4a74bb4af4
Joe Prakash <joep@nyu.edu>
2024-12-16 10:23:16 -0500

    mv auto key gen

--- apps/api/models.py
+++ apps/api/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 from django.utils import timezone
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.conf import settings
 from os import urandom
 from binascii import hexlify
+
 from core.model_fields import UUID7StringField
 
 from .utils import extract_app_name
@@ -34,6 +37,10 @@ class AccessToken(models.Model):
     def generate_key(self):
         return hexlify(urandom(20)).decode()
 
+    def rotate_key(self):
+        self.key = self.generate_key()
+        self.save()
+
     def update_last_used(self):
         self.last_used = timezone.now()
         self.save()
@@ -51,14 +58,20 @@ class AccessToken(models.Model):
         is_write_method = method in ('PUT', 'PATCH', 'DELETE')
         allowed = False
 
-        if 'global' in self.scopes:
-            allowed = True
-
-        app_name = extract_app_name(url_path)
-        if app_name in self.scopes:
+        if ('global' in self.scopes) or (extract_app_name(url_path) in self.scopes):
             allowed = True
 
         return (allowed and self.can_write) if is_write_method else allowed
 
     def is_expired(self):
         return self.expiration and self.expiration <= timezone.now()
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        AccessToken.objects.create(user=instance,
+                                   name='SYS_KEY',
+                                   scopes=['container_registry'],
+                                   can_write='True',
+                                   system_use=True)
