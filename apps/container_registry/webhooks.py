7010c795f18fe7c8ade8ca479939f2fdd764fce2
Joe Prakash <joep@nyu.edu>
2024-12-30 00:57:46 -0500

    fix webhook issues

--- apps/container_registry/webhooks.py
+++ apps/container_registry/webhooks.py
@@ -1,25 +1,32 @@
 import logging
 
 from django.http import JsonResponse
-from .models import RegistryWebhook
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
+from json import loads as json_loads
+
+from .models import RegistryWebhook, ContainerRegistry
+
 from core.utils import success_message, error_message
 from core.settings import env
 
 
+@csrf_exempt
+@require_http_methods(["POST"])
 def registry_webhook(request):
-    if request.method != 'POST':
-        return JsonResponse(error_message('Not allowed'), status=400)
     try:
         auth = request.headers.get('Authorization')
-        if auth != env.registry_webhook_token:
+        if auth != env.registry_webhook_secret:
             return JsonResponse(error_message('Unauthorized'), status=401)
 
-        events = request.data.get('events')
+        post_data = json_loads(request.body.decode())
+        events = post_data['events']
         for event in events:
-            RegistryWebhook.objects.create(repository=event['target']['repository'],
-                                           action=event['action'],
-                                           content=event
-                                           ).save()
+            webhook = RegistryWebhook(action=event['action'], content=event)
+            repo, target = event['target']['repository'].split('/', 1)
+            webhook.registry = ContainerRegistry.objects.get(repo=repo)
+            webhook.target = target
+            webhook.save()
 
         return JsonResponse(success_message('Webhook processed'), status=200)
     except Exception as e:
