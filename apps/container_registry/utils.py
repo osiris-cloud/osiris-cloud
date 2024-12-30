79bb6621d3b4e02cd7d782294bc585adfa2bd0eb
Joe Prakash <joep@nyu.edu>
2024-12-30 00:59:58 -0500

    fix repo path

--- apps/container_registry/utils.py
+++ apps/container_registry/utils.py
@@ -1,6 +1,5 @@
 import logging
 
-from core.utils import error_message
 from regex import match
 from httpx import AsyncClient
 from datetime import datetime, timedelta, timezone
@@ -13,40 +12,42 @@ from ..k8s.constants import DOCKER_HEADERS
 CATALOG_TOKEN = None
 
 
-def validate_registry_spec(spec: dict) -> tuple[bool, dict | None]:
+def validate_registry_spec(spec: dict) -> tuple[bool, str | None]:
     """
     Validate registry spec
     """
+    if not spec:
+        return False, 'Empty request'
+
     if len(name := spec.get('name', '')) < 1 or len(name) > 64:
-        return False, error_message('Name should be between 1 and 64 characters')
+        return False, 'Name should be between 1 and 64 characters'
 
-    slug = spec.get('slug')
-    if not slug:
-        return False, error_message('Slug is required')
+    repo_name = spec.get('repo_name', '').strip()
+    if not repo_name:
+        return False, 'Repo is required'
 
-    if len(slug) < 3 or len(slug) > 32:
-        return False, error_message('Slug should be between 3 and 32 characters')
+    if not match(r'^[a-z0-9]+(-[a-z0-9]+)*$', repo_name):
+        return False, 'Invalid repo name'
 
-    if not match(r'^[a-z0-9]+(-[a-z0-9]+)*$', slug):
-        return False, error_message('Invalid slug')
-
-    if len(spec.get('password', '')) < 8:
-        return False, error_message('Password must be at least 8 characters')
+    if spec.get('public', False) not in (True, False):
+        return False, 'Public should be a boolean'
 
     return True, None
 
 
-def validate_registry_update_spec(spec: dict) -> tuple[bool, dict | None]:
+def validate_registry_update_spec(spec: dict) -> tuple[bool, str | None]:
     """
     Validate registry update spec
     """
+    if not spec:
+        return False, 'Empty request'
+
     if name := spec.get('name', ''):
         if len(name) < 1 or len(name) > 64:
-            return False, error_message('Name should be between 1 and 64 characters')
+            return False, 'Name should be between 1 and 64 characters'
 
-    if psw := spec.get('password', ''):
-        if len(psw) < 8:
-            return False, error_message('Password must be at least 8 characters')
+    if spec.get('public', False) not in (True, False):
+        return False, 'Public should be a boolean'
 
     return True, None
 
@@ -67,7 +68,7 @@ def generate_auth_token(r_type: str | None = None, r_name: None | str = None, ac
     }
     claim = {
         'iss': 'OCR',
-        'sub': 'osiris',
+        'sub': '',
         'aud': 'OCR',
         'exp': int((now + timedelta(**kwargs)).timestamp()),
         'nbf': int((now - timedelta(seconds=30)).timestamp()),
@@ -117,7 +118,7 @@ async def get_all_repositories() -> tuple[str]:
     headers = {**DOCKER_HEADERS, 'Authorization': f"Bearer {CATALOG_TOKEN}"}
     url = f"https://{env.registry_domain}/v2/_catalog"
 
-    async with AsyncClient(headers=headers) as client:
+    async with AsyncClient(headers=headers, verify=False) as client:
         try:
             resp = await client.get(url)
             data = resp.json()
@@ -149,8 +150,8 @@ async def get_tags(client: AsyncClient, repo_path: str) -> list[str]:
         return []
 
 
-async def get_manifest(client: AsyncClient, sub_repo: str, tag: str) -> dict:
-    url = f"https://{env.registry_domain}/v2/{sub_repo}/manifests/{tag}"
+async def get_manifest(client: AsyncClient, repo_path: str, tag: str) -> dict:
+    url = f"https://{env.registry_domain}/v2/{repo_path}/manifests/{tag}"
     try:
         resp = await client.get(url)
         manifest = resp.json()
@@ -166,9 +167,9 @@ def get_blob_digests(manifest: dict) -> list[str]:
     return [layer.get('digest', '') for layer in layers] + [manifest.get('config', {}).get('digest', '')]
 
 
-async def delete_blob(client: AsyncClient, sub_repo: str, digest: str) -> bool:
+async def delete_blob(client: AsyncClient, repo_path: str, digest: str) -> bool:
     try:
-        resp = await client.delete(f"https://{env.registry_domain}/v2/{sub_repo}/blobs/{digest}")
+        resp = await client.delete(f"https://{env.registry_domain}/v2/{repo_path}/blobs/{digest}")
         return resp.status_code == 202
     except Exception as e:
         logging.exception(e)
