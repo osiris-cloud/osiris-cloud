918f891bdf61a8e68d85ffb7af8a0c80ed90892c
Joe Prakash <joep@nyu.edu>
2025-01-03 22:56:24 -0500

    modify state and time fields

--- apps/secret_store/models.py
+++ apps/secret_store/models.py
@@ -7,6 +7,7 @@ from ..k8s.models import Namespace
 from ..k8s.constants import R_STATES, SECRET_TYPES
 
 from core.model_fields import UUID7StringField
+from core.utils import similar_time
 
 
 class Secret(models.Model):
@@ -17,7 +18,6 @@ class Secret(models.Model):
     namespace = models.ForeignKey(Namespace, on_delete=models.CASCADE, related_name='secrets')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    state = models.CharField(max_length=16, choices=R_STATES, default='creating')
 
     def info(self):
         return {
@@ -26,11 +26,16 @@ class Secret(models.Model):
             'type': self.type,
             'created_at': self.created_at,
             'updated_at': self.updated_at,
-            'state': self.state,
         }
 
     def values(self):
         return json_loads(self.data) if self.data else {}
+
+    @property
+    def updated_at_pretty(self):
+        if similar_time(self.created_at, self.updated_at):
+            return 'Never'
+        return self.updated_at
 
     class Meta:
         db_table = 'secret_store'
