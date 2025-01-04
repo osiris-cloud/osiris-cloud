cb2c2e91dafa3d5182e98cb0ee90dd288d14c415
Joe Prakash <joep@nyu.edu>
2025-01-03 22:54:25 -0500

    optimize data retrieval

--- apps/secret_store/api.py
+++ apps/secret_store/api.py
@@ -1,7 +1,6 @@
 import logging
 
 from django.http import JsonResponse
-from django.core.exceptions import ValidationError
 from rest_framework.decorators import api_view
 
 from json import dumps as json_dumps
@@ -10,11 +9,9 @@ from json import JSONDecodeError
 from .models import Secret
 from ..k8s.models import Namespace
 
-from core.utils import success_message, error_message, serialize_obj
+from core.utils import success_message, error_message
 from ..users.utils import get_default_ns
 from .utils import validate_secret_creation, validate_secret_update
-
-from .tasks import create_secret, update_secret, delete_secret
 
 
 @api_view(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
@@ -32,20 +29,16 @@ def secret_store(request, nsid=None, secretid=None, action=None):
     if (request.method in ['PATCH, DELETE']) and secretid is None:
         return JsonResponse(error_message('secretid is required'), status=400)
 
-    if (request.user.role in ('guest', 'blocked')) and (request.method in ('PUT', 'PATCH', 'DELETE')):
-        return JsonResponse(error_message('Permission denied'), status=403)
-
     try:
-        ns = Namespace.objects.filter(nsid=nsid).first()
-        if ns is None:
-            return JsonResponse(error_message('Namespace not found or no permission to access'), status=404)
-
+        ns = Namespace.objects.get(nsid=nsid)
         role = ns.get_role(request.user)
         if role is None:
             return JsonResponse(error_message('Namespace not found or no permission to access'), status=404)
 
-        secret_data = request.data
+        if request.method in ('PUT', 'PATCH', 'DELETE') and role == 'viewer':
+            return JsonResponse(error_message('Permission denied'), status=403)
 
+        secret_data = request.data
         match request.method:
             case 'GET':
                 secrets_filter = {'secretid': secretid} if secretid else {}
@@ -59,22 +52,13 @@ def secret_store(request, nsid=None, secretid=None, action=None):
                 return JsonResponse(success_message('Get secrets', {'secrets': result}), status=200)
 
             case 'POST':
-                if role == 'viewer':
-                    return JsonResponse(error_message('Permission denied'), status=403)
-
-                secret = Secret.objects.filter(namespace=ns, secretid=secretid).first()
-                if not secret:
-                    return JsonResponse(error_message('Secret not found'), status=404)
-
+                secret = Secret.objects.get(namespace=ns, secretid=secretid)
                 if action == 'values':
                     return JsonResponse(success_message('Get secret values', {'values': secret.values()}), status=200)
 
                 return JsonResponse(error_message('Invalid action'), status=400)
 
             case 'PUT':
-                if role not in ['owner', 'manager']:
-                    return JsonResponse(error_message('Permission denied'), status=403)
-
                 valid, err = validate_secret_creation(secret_data)
                 if not valid:
                     return JsonResponse(error_message(err), status=400)
@@ -90,49 +74,40 @@ def secret_store(request, nsid=None, secretid=None, action=None):
                 )
 
                 secret.save()
-                create_secret.delay(serialize_obj(secret))
 
-                return JsonResponse(success_message('Create Secret', secret.info()))
+                return JsonResponse(success_message('Create Secret', {'secret': secret.info()}))
 
             case 'PATCH':
-                if role not in ['owner', 'manager']:
-                    return JsonResponse(error_message('Permission denied'), status=403)
-
                 valid, err = validate_secret_update(secret_data)
                 if not valid:
                     return JsonResponse(error_message(err))
 
-                secret = Secret.objects.filter(namespace=ns, secretid=secretid).first()
-                if not secret:
-                    return JsonResponse(error_message('Secret not found'), status=404)
-
+                secret = Secret.objects.get(namespace=ns, secretid=secretid)
                 if secret_name := secret_data.get('name'):
                     secret.name = secret_name
-                if secret_values := secret_data.get('values'):
+
+                secret_values = secret_data.get('values')
+                if secret_values is not None:
                     secret.data = json_dumps(secret_values)
 
                 secret.save()
-                update_secret.delay(serialize_obj(secret))
 
-                return JsonResponse(success_message('Update Secret', secret.info()))
+                return JsonResponse(success_message('Update Secret', {'secret': secret.info()}))
 
             case "DELETE":
-                if role not in ['owner', 'manager']:
-                    return JsonResponse(error_message('Permission denied'), status=403)
-
-                secret = Secret.objects.filter(namespace=ns, secretid=secretid).first()
-                if not secret:
-                    return JsonResponse(error_message('Secret not found'), status=404)
-
-                delete_secret.delay(serialize_obj(secret))
+                secret = Secret.objects.get(namespace=ns, secretid=secretid)
+                secret.delete()
 
                 return JsonResponse(success_message('Delete Secret'))
 
+    except Namespace.DoesNotExist:
+        return JsonResponse(error_message('Namespace not found or no permission to access'), status=404)
+
+    except (Secret.DoesNotExist, ValueError):
+        return JsonResponse(error_message('Secret not found'), status=404)
+
     except JSONDecodeError:
         return JsonResponse(error_message('Invalid JSON data'), status=400)
-
-    except (ValueError, ValidationError) as e:
-        return JsonResponse(error_message('Unexpected secretid/input data type'), status=400)
 
     except Exception as e:
         logging.exception(e)
