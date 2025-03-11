4de5b89594ff064a0744637cc8bd1201f1dbf046
Joe Prakash <joep@nyu.edu>
2025-03-10 21:23:23 -0400

    add app urls

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -6,7 +6,8 @@ from . import routes
 # from ..vm import consumers as vm_consumers
 
 from ..users import api as user_api
-from ..users import consumers as user_consumers
+from ..users.consumers import UserSearchConsumer
+from ..container_apps.consumers import AppConsumer, TerminalConsumer, LogsConsumer
 
 from ..infra import api as k8s_api
 
@@ -83,5 +84,10 @@ urlpatterns = (
 
 websocket_urlpatterns = (
     # re_path(r'^api/vnc/(?P<vmid>[^/]+)$', vm_consumers.VNCProxyConsumer.as_asgi()),
-    re_path(r'^api/user/search$', user_consumers.UserSearchConsumer.as_asgi()),
+    re_path(r'^api/user/search$', UserSearchConsumer.as_asgi()),
+    re_path(r'^api/container-apps/(?P<nsid>[^/]+)/(?P<appid>[^/]+)/shell/(?P<iref>[^/]+)/(?P<cref>[^/]+)$',
+            TerminalConsumer.as_asgi()),
+    re_path(r'^api/container-apps/(?P<nsid>[^/]+)/(?P<appid>[^/]+)/logs/(?P<iref>[^/]+)/(?P<cref>[^/]+)$',
+            LogsConsumer.as_asgi()),
+    re_path(r'^api/container-apps/(?P<nsid>[^/]+)/(?P<appid>[^/]+)$', AppConsumer.as_asgi()),
 )
